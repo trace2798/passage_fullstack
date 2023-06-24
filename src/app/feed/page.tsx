ba283cc1341a31c91db9f030ad9f64a691f17f10
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-06-25 00:15:38 +0530

    added conditional return on feed page

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,10 +1,29 @@
+import { getUserInfo } from "@/actions/getUserInfo";
 import { PostDialog } from "@/components/Dialog";
 import GeneralFeed from "@/components/homepage/GeneralFeed";
 import { FC } from "react";
 
 interface feedProps {}
 
-const feed: FC<feedProps> = ({}) => {
+const feed: FC<feedProps> = async ({}) => {
+  const id = await getUserInfo();
+  const userId = id.props.userID;
+
+  if (!userId) {
+    return (
+      <>
+        <main className="flex min-h-screen flex-col items-center justify-between p-10">
+          <div className="items-center">
+            <h1 className="font-bold text-4xl text-center">
+              Discover Milestones
+            </h1>
+          </div>
+
+          <GeneralFeed />
+        </main>
+      </>
+    );
+  }
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-between p-10">
