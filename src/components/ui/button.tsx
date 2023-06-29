a1aa2f62381497d9047d5e8e995958dfec2e7555
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-06-29 14:38:46 +0530

    added hover for button on light mode

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground dark:hover:bg-slate-900 dark:hover:text-white",
+          "bg-primary text-primary-foreground hover:bg-neutral-100 hover:text-slate-800  dark:hover:bg-slate-900 dark:hover:text-white",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
